54d44b7d2b77a7b7db27ebd6a35a6aef3567481d
acharyarupak391 <acharyarupak391@gmail.com>
2021-11-19 17:07:06 +0545

    package.json edited

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.use(express.json());
 app.use(serverRoute)
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 );

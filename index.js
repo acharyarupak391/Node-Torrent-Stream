ac9176a22cc83bc308c95231360c33d5e3d1aad4
acharyarupak391 <acharyarupak391@gmail.com>
2022-12-14 15:27:50 +0000

    Updated stream implementation by using 'range-parser' library

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 
-var cors = require('cors')
+var cors = require("cors");
 
-const serverRoute = require('./routes')
+const serverRoute = require("./routes");
 
 const port = process.env.PORT || 80;
 
-app.use(cors())
+// app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-app.use(serverRoute)
+app.use(serverRoute);
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const WebTorrent = require("webtorrent");
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+const rangeParser = require('range-parser')
 
 class Torrent {
   constructor(magnetOrBuffer, res) {
@@ -15,6 +16,7 @@ class Torrent {
 
   listenForError(res) {
     this.client.on("error", (err) => {
+      console.log({err})
       res.status(400).json({ error: err.message });
     });
   }
@@ -40,46 +42,76 @@ class Torrent {
     });
   }
 
+  // stream(index, req, res) {
+  //   if (!this.torrent) {
+  //     return res.status(400).json({ message: "Unable to stream torrent" });
+  //   }
+  //   const sfi = this.torrent.files.findIndex(file => file.name.endsWith(".mp4"))
+  //   let fileSize = this.torrent.files[index].length;
+  //   if (!req.headers.range) {
+	// 		const head = {
+	// 			"Content-Length": fileSize,
+  //       "Content-Type": "video/mp4",
+  //     };
+  //     res.writeHead(200, head);
+	// 		let readStream1 = this.torrent.files[index].createReadStream();
+  //     readStream1.pipe(res);
+
+	// 		readStream1.on('end', () => {
+	// 			console.log('first stream end')
+	// 		})
+  //   } else {
+	// 		console.log("range: ", req.headers.range);
+  //     const range = req.headers.range;
+  //     const parts = range.replace(/bytes=/, "").split("-");
+  //     const start = parseInt(parts[0], 10);
+  //     const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+  //     const chunksize = end - start + 1;
+  //     let readStream2 = this.torrent.files[index].createReadStream({start, end});
+  //     const head = {
+	// 			"Content-Range": `bytes ${start}-${end}/${fileSize}`,
+  //       "Accept-Ranges": "bytes",
+  //       "Content-Length": chunksize,
+  //       "Content-Type": "video/mp4",
+  //     };
+  //     res.writeHead(206, head);
+  //     readStream2.pipe(res);
+
+	// 		readStream2.on('end', () => {
+
+	// 			console.log('second stream end')
+	// 		})
+  //   }
+    
+  // }
+
   stream(index, req, res) {
     if (!this.torrent) {
       return res.status(400).json({ message: "Unable to stream torrent" });
     }
-    const sfi = this.torrent.files.findIndex(file => file.name.endsWith(".mp4"))
+
     let fileSize = this.torrent.files[index].length;
-    if (!req.headers.range) {
-			const head = {
-				"Content-Length": fileSize,
-        "Content-Type": "video/mp4",
-      };
-      res.writeHead(200, head);
-			let readStream1 = this.torrent.files[index].createReadStream();
-      readStream1.pipe(res);
+    // Parse the Range header to get the start and end bytes
+    const range = rangeParser(fileSize, req.headers.range)
+    let readStream;
 
-			readStream1.on('end', () => {
-				console.log('first stream end')
-			})
+    if(range) {
+      // Set the Content-Range and Content-Length headers
+      res.set('Content-Range', `bytes ${range.start}-${range.end}/${fileSize}`)
+      res.set('Content-Length', range.end - range.start + 1)
+  
+      // Create a read stream for the file and pipe it directly to the response
+      readStream = this.torrent.files[index].createReadStream({start: range.start, end: range.end});
     } else {
-			console.log("range: ", req.headers.range);
-      const range = req.headers.range;
-      const parts = range.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-      const chunksize = end - start + 1;
-      let readStream2 = this.torrent.files[index].createReadStream({start, end});
-      const head = {
-				"Content-Range": `bytes ${start}-${end}/${fileSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4",
-      };
-      res.writeHead(206, head);
-      readStream2.pipe(res);
-
-			readStream2.on('end', () => {
-
-				console.log('second stream end')
-			})
+      // The Range header is not present, so send the entire file
+      res.set('Content-Length', fileSize)
+      readStream = this.torrent.files[index].createReadStream();
     }
+    readStream.pipe(res)
+
+    readStream.on('end', () => {
+      console.log('second stream end')
+    })
     
   }
 	
